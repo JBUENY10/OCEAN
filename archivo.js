aa117587e8b017acea2d00a9051bb80cfbae8a5c
JBUENY10 <161310219+JBUENY10@users.noreply.github.com>
2024-04-09 13:33:35 +0200

    Add files via upload

---
+++ archivo.js
@@ -0,0 +1,8 @@
+function imgoscura (){
+document.getElementById ("github").src = "/img/github_n.png"
+}
+
+function imgclara (){
+    document.getElementById ("github").src = "/img/github2.svg.png"
+}   
+    
